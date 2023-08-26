627dd3a32ca8467242e910f3b2b5b60b842c6c94
itwasjoke <andreyvasiliev1204@gmail.com>
2023-08-26 19:12:24 +0300

    added getters and setters

--- src/main/java/com/eturn/domain/User.java
+++ src/main/java/com/eturn/domain/User.java
@@ -3,7 +3,6 @@ package com.eturn.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 
-@Getter
 @Entity
 @Table
 public class User {
@@ -16,4 +15,36 @@ public class User {
     private Long idGroup;
 
     private int status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getIdGroup() {
+        return idGroup;
+    }
+
+    public void setIdGroup(Long idGroup) {
+        this.idGroup = idGroup;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
