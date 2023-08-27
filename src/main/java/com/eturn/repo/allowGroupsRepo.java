569b282648f5f538e6cb4010c556f2228ddf0da4
sbloh <sblohin8@gmail.com>
2023-08-27 18:54:05 +0300

    .

---
+++ src/main/java/com/eturn/repo/allowGroupsRepo.java
@@ -0,0 +1,8 @@
+package com.eturn.repo;
+
+import com.eturn.domain.Position;
+import com.eturn.domain.allowGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface allowGroupsRepo extends JpaRepository<allowGroup,Long> {
+}
