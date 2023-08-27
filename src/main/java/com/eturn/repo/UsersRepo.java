610033a8207da3f82dc10345802fc98a5f254196
sbloh <sblohin8@gmail.com>
2023-08-27 18:48:13 +0300

    Изменил название

--- src/main/java/com/eturn/repo/UsersRepo.java
+++ src/main/java/com/eturn/repo/UsersRepo.java
@@ -3,5 +3,6 @@ package com.eturn.repo;
 import com.eturn.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UsersRepo extends JpaRepository<User, Long> {
+
 }
