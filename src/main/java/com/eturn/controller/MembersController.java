d1fc0ff27fc5785516b07c2a447e75d5be81de06
sbloh <sblohin8@gmail.com>
2023-08-27 18:50:44 +0300

    Имя изменил

---
+++ src/main/java/com/eturn/controller/MembersController.java
@@ -0,0 +1,35 @@
+package com.eturn.controller;
+
+
+import com.eturn.domain.Member;
+import com.eturn.repo.MembersRepo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("member")
+public class MembersController {
+
+    private final MembersRepo membersRepo;
+
+
+    public MembersController(MembersRepo membersRepo) {this.membersRepo = membersRepo;}
+
+    @GetMapping
+    public List<Member> getMemberList(){
+        return membersRepo.findAll();
+    }
+
+    @PostMapping
+    public Member create(@RequestBody Member member){
+        return membersRepo.save(member);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Member member) {membersRepo.delete(member);};
+
+
+
+
+}
