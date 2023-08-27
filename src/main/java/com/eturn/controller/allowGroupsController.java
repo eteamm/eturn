b53174f44a15fb541617b125e6771fb2e5a4a73b
sbloh <sblohin8@gmail.com>
2023-08-27 18:54:09 +0300

    .

---
+++ src/main/java/com/eturn/controller/allowGroupsController.java
@@ -0,0 +1,37 @@
+package com.eturn.controller;
+
+import com.eturn.domain.Group;
+import com.eturn.domain.allowGroup;
+import com.eturn.repo.allowGroupsRepo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("allowGroup")
+public class allowGroupsController {
+
+    private final allowGroupsRepo AllowGroupsRepo;
+
+    public allowGroupsController(allowGroupsRepo allowGroupRepo) {
+        this.AllowGroupsRepo = allowGroupRepo;
+    }
+
+
+    @GetMapping
+    public List<allowGroup> getAllowGroupList(){
+        return AllowGroupsRepo.findAll();
+    }
+
+    @PostMapping
+    public allowGroup create(@RequestBody allowGroup AllowGroup){
+        return AllowGroupsRepo.save(AllowGroup);
+
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") allowGroup AllowGroup) {AllowGroupsRepo.delete(AllowGroup);}
+
+
+}
