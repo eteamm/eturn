303a15c54eb6eab0394761dd3dab0273c623a084
sbloh <sblohin8@gmail.com>
2023-08-30 00:12:52 +0300

    Удаление пользователя

--- src/main/java/com/eturn/controller/UsersController.java
+++ src/main/java/com/eturn/controller/UsersController.java
@@ -1,19 +1,31 @@
 package com.eturn.controller;
 
 
+import com.eturn.domain.Turn;
 import com.eturn.domain.User;
-import com.eturn.repo.UsersRepo;
+import com.eturn.repo.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 @RestController
 @RequestMapping("user")
 public class UsersController {
     private final UsersRepo usersRepo;
+    private final MembersRepo membersRepo;
+    private final PositionsRepo positionsRepo;
+    private final TurnsRepo turnsRepo;
+    private final AllowGroupsRepo allowGroupsRepo;
 
-    public UsersController(UsersRepo usersRepo) {
+    public UsersController(UsersRepo usersRepo, MembersRepo membersRepo, PositionsRepo positionsRepo,
+                           TurnsRepo turnsRepo, AllowGroupsRepo allowGroupsRepo) {
+
         this.usersRepo = usersRepo;
+        this.membersRepo=membersRepo;
+        this.positionsRepo=positionsRepo;
+        this.turnsRepo=turnsRepo;
+        this.allowGroupsRepo=allowGroupsRepo;
     }
 
     @GetMapping
@@ -26,11 +38,24 @@ public class UsersController {
 
     @DeleteMapping("{id}")
     public void delete(@PathVariable("id") User user){
+        membersRepo.deleteByIdUser(user.getId());
+        positionsRepo.deleteByIdUser(user.getId());
+        List<Turn> turns=turnsRepo.findByIdUser(user.getId());
+        turns.forEach(new Consumer<Turn>(){
+            @Override
+            public void accept(Turn turn){
+                allowGroupsRepo.deleteByIdTurn(turn.getId());
+                membersRepo.deleteByIdTurn(turn.getId());
+                positionsRepo.deleteByIdTurn(turn.getId());
+                turnsRepo.delete(turn);
+            }
+
+        });
+
+
         usersRepo.delete(user);
-        //проверка статутса текущего пользователя
-        //если админ то, что будет с turns?
     }
-    // тут надо удалить: members, positions, groups, turns
+
 
 
 }
