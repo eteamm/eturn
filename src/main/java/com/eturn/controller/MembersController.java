9a9497442fd98814866ed8747f2f3232b469f4ab
sbloh <sblohin8@gmail.com>
2023-08-30 00:13:00 +0300

    Изменение прав

--- src/main/java/com/eturn/controller/MembersController.java
+++ src/main/java/com/eturn/controller/MembersController.java
@@ -2,9 +2,11 @@ package com.eturn.controller;
 
 
 import com.eturn.domain.Member;
+import com.eturn.domain.Turn;
 import com.eturn.domain.User;
 import com.eturn.repo.MembersRepo;
 import com.eturn.repo.PositionsRepo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,9 +39,19 @@ public class MembersController {
         member.setRoot(0);
         return membersRepo.save(member);
     }
+    @PutMapping("{id_turn}/{id_user}")
+    public Member update(@RequestBody Member member,@PathVariable("id_turn") Long id_turn,@PathVariable("id_user") Long id_user){
+        Member memberFromDb =membersRepo.getByIdUserAndIdTurn(id_user,id_turn);
+
+        if (member.getRoot()>=0 && member.getRoot()<=2)
+        {
+            BeanUtils.copyProperties(member, memberFromDb, "id");
+            return memberFromDb;
+        }
+        return memberFromDb;
+    }
 
 
-    //Добавить изменение прав
 
     @DeleteMapping("{id_turn}/{id_user}")
     public void delete(@PathVariable("id_user") Long id_user, @PathVariable("id_turn") Long id_turn) {
